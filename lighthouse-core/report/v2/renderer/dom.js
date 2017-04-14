72ac08ebea46a28c314c1a380833891b5f22b8ad
Eric Bidelman <ebidel@gmail.com>
2017-04-13 18:04:16 -0700

    Typo

--- lighthouse-core/report/v2/renderer/dom.js
+++ lighthouse-core/report/v2/renderer/dom.js
@@ -29,7 +29,7 @@ class DOM {
    * @param {string} name
    * @param {string=} className
    * @param {!Object<string, (string|undefined)>=} attrs Attribute key/val pairs.
-   *     Note: uf an attribute key has an undefined value, this method does not
+   *     Note: if an attribute key has an undefined value, this method does not
    *     set the attribute on the node.
    * @return {!Element}
    */
